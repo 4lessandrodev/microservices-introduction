3776eb7ef6e94aa2c4e6886dc46136399572bbff
Alessandrodev <alessandroadm@live.com>
2021-04-01 01:42:59 -0300

    feat: inject modules and mongo connection on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PlayerModule } from './player/player.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://mongo_user:mongo_pass@localhost:27017/player_db?authSource=admin',
+      {
+        connectTimeoutMS: 300,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      },
+    ),
+    PlayerModule,
+    CategoryModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
