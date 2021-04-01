3c449054186e1d59d1a6a6b87a1783b352f6dadc
Alessandrodev <alessandroadm@live.com>
2021-04-01 01:43:28 -0300

    feat: add player schema

---
+++ src/player/entities/player.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type PlayerDocument = Player & Document;
+
+@Schema({ autoCreate: true, timestamps: true })
+export class Player {
+  @Prop()
+  name: string;
+
+  @Prop({ unique: true, type: String })
+  email: string;
+
+  @Prop({ type: String })
+  phone: string;
+
+  @Prop({ type: String })
+  ranking: string;
+
+  @Prop({ type: Number })
+  posicaoRanking: number;
+
+  @Prop({ type: String })
+  urlFotoJogador: string;
+}
+
+export const PlayerSchema = SchemaFactory.createForClass(Player);
