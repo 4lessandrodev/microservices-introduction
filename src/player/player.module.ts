714e1d46ece9af593169353f5493ff7b918296c5
Alessandrodev <alessandroadm@live.com>
2021-04-01 01:41:28 -0300

    feat: add player module

---
+++ src/player/player.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Player, PlayerSchema } from './entities/player.schema';
+import { PlayerController } from './player.controller';
+import { PlayerRepository } from './player.repository';
+import { PlayerService } from './player.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Player.name, schema: PlayerSchema }]),
+  ],
+  controllers: [PlayerController],
+  providers: [PlayerRepository, PlayerService],
+})
+export class PlayerModule {}
