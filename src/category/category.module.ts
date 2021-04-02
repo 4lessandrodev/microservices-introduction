fb850065468260d6cbfe3079eef6cc26b69d6212
Alessandrodev <alessandroadm@live.com>
2021-04-02 02:18:21 -0300

    feat: inject player module to category to use player repository

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { CategoryRepository } from './category.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from './entities/category.schema';
+import { PlayerModule } from '../player/player.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Category.name, schema: CategorySchema },
+    ]),
+    PlayerModule,
+  ],
   controllers: [CategoryController],
-  providers: [CategoryService]
+  providers: [CategoryRepository, CategoryService],
 })
 export class CategoryModule {}
