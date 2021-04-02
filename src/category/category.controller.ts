7fe311d43cab0738a438753c45b5761be87dd043
Alessandrodev <alessandroadm@live.com>
2021-04-02 02:17:16 -0300

    feat: add category controller

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,12 +1,64 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CustomParamPipe } from '../common/pipes/custom-param.pipe';
 import { CategoryService } from './category.service';
+import { AddPlayerToCategoryDto } from './dto/add-player-to-category.dto';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from './entities/category.schema';
 
-@Controller('categories')
+@Controller('/api/v1/categories')
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+  constructor(
+    @Inject(CategoryService)
+    private readonly categoryService: CategoryService,
+  ) {}
+
+  @Post()
+  @UsePipes(CreateCategoryDto)
+  createCategory(@Body() createCategoryDto: CreateCategoryDto): Promise<void> {
+    return this.categoryService.saveCategory(createCategoryDto);
+  }
 
   @Get()
-  getHello() {
-    return `Hello World`;
+  getCategories(): Promise<Array<Category>> {
+    return this.categoryService.getCategories();
+  }
+
+  @Get('/:category')
+  getCategoryById(@Param(CustomParamPipe) category: string): Promise<Category> {
+    return this.categoryService.findByCategory(category);
+  }
+
+  @Put('/:_id')
+  updateCategory(
+    @Body(ValidationPipe) updateCategoryDto: UpdateCategoryDto,
+    @Param(CustomParamPipe) _id: string,
+  ): Promise<void> {
+    updateCategoryDto._id = _id;
+    return this.categoryService.updateCategory(updateCategoryDto);
+  }
+
+  @Delete('/:_id')
+  deleteCategory(@Param(CustomParamPipe) _id: string): Promise<void> {
+    return this.categoryService.deleteCategory(_id);
+  }
+
+  @Patch('/:category/:playerId')
+  addPlayerToCategory(
+    @Param() addPlayerToCategoryDto: AddPlayerToCategoryDto,
+  ): Promise<void> {
+    return this.categoryService.addPlayerOnCategory(addPlayerToCategoryDto);
   }
 }
