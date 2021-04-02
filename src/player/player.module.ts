c56da45cbe139f50c42dd2156352da8b55b67f70
Alessandrodev <alessandroadm@live.com>
2021-04-02 02:21:43 -0300

    feat: add export repo to player

--- src/player/player.module.ts
+++ src/player/player.module.ts
@@ -11,5 +11,6 @@ import { PlayerService } from './player.service';
   ],
   controllers: [PlayerController],
   providers: [PlayerRepository, PlayerService],
+  exports: [PlayerRepository],
 })
 export class PlayerModule {}
